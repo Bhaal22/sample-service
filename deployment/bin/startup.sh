80df4563023a4f8deb9e31493e5b6f581e8d6a34
Kabir Sohel <kabir.sohel@flowbird.group>
2019-05-28 15:42:06 +0200

    BPP-980 : Fixed deployment with config file location

--- deployment/bin/startup.sh
+++ deployment/bin/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar -Dserver.port=80 -Dspring.profiles.active=SIT-BE /usr/share/yb-payment-service/bin/payment-service-0.0.1-SNAPSHOT.jar --spring.config.location=../config/ &
+java -jar -Dserver.port=80 -Dspring.profiles.active=SIT-BE /usr/share/yb-payment-service/bin/payment-service-0.0.1-SNAPSHOT.jar --spring.config.location="file:/usr/share/yb-payment-service/config/,classpath:/" &
